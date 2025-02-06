ce907afaf70019f8a0831aade84e4d37e9cda4d9
Gus <jianbaiye@outlook.com>
2025-02-06 17:47:48 +0800

    WIP

--- src/server/api/memobase_server/controllers/modal/chat/extract.py
+++ src/server/api/memobase_server/controllers/modal/chat/extract.py
@@ -50,13 +50,6 @@ async def extract_topics(
         already_topics_prompt = ""
 
     blob_strs = tag_chat_blobs_in_order_xml(blobs)
-    print(
-        PROMPTS[CONFIG.language]["extract"].pack_input(
-            already_topics_prompt,
-            blob_strs,
-            PROMPTS[CONFIG.language]["extract"].get_default_profiles(),
-        ),
-    )
     p = await llm_complete(
         PROMPTS[CONFIG.language]["extract"].pack_input(
             already_topics_prompt,
